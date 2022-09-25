4ab5bd757b362e718531bb2672cea21c79f4ac3f
Harris <hjubb@users.noreply.github.com>
2022-09-26 00:13:15 +1000

    feat: remove unused commented out code

--- config/index.ts
+++ config/index.ts
@@ -7,9 +7,5 @@ if (!process.env.NFT_STORAGE_KEY) {
     throw new Error("Please set your NFT_STORAGE_KEY in a .env file");
 }
 
-// if (!process.env.NFT_CONTRACT) {
-//     throw new Error("Please set your deployed contract's NFT_CONTRACT address in a .env file");
-// }
-
 export const nftStorageKey: string = process.env.NFT_STORAGE_KEY;
 export const nftAddress: string = process.env.NFT_CONTRACT!!;
